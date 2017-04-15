b7f80749d6cb19fbf38e808ff5a2e852f614347b
Michal Klimczak <klimkomania@interia.pl>
2017-04-15 08:22:51 +0200

    add plugin contrib-watch

--- gruntfile.js
+++ gruntfile.js
@@ -13,19 +13,29 @@ grunt.initConfig({
       }
     },
     imagemin: {
-    dynamic: {
-        files: [{
-            expand: true,
-            cwd: 'images/',
-            src: ['**/*.{png,jpg,gif}'],
-            dest: 'images/build/'
-        	}]
-    	}
+	    dynamic: {
+	        files: [{
+	            expand: true,
+	            cwd: 'images/',
+	            src: ['**/*.{png,jpg,gif}'],
+	            dest: 'images/build/'
+	       	}]
+	    }
 	}
+	watch: {
+	    scripts: {
+	        files: ['sass/*.sass'],
+	        tasks: ['sass'],
+	        options: {
+	            spawn: false,
+	        },
+	    } 
+	}, 
 });
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   // Default task(s).
-  grunt.registerTask('default', ["sass", "imagemin"]);
+  grunt.registerTask('default', ["sass", "imagemin", "watch"]);
 };
